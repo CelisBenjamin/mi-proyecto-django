c3aecc5d6b4ae57a03c901ba9ac17bb482ef5a64
Benjamín Celis <benjamin.celisd@gmail.com>
2025-12-23 16:03:40 -0300

    Último ajuste

--- mi_primera_web/settings.py
+++ mi_primera_web/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-%ctnup+35!*5nqs$iwezjqnoe%%k21^7)o@$rv%z^_&=*kj#xk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # Esto quita esa pantalla técnica y pone una elegante de "Error 404" o "500"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mi-proyecto-django-4bv2.onrender.com']
 
 
 # Application definition
